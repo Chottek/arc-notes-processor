26f3aaad3cca3ea2b7ca717a0eef869ec7de0a49
Chottek <49697145+Chottek@users.noreply.github.com>
2020-11-16 11:19:38 +0100

    Changed structure a lil' bit

---
+++ src/main/java/pl/fox/arcnotes/ArcNotesProcessorApplication.java
@@ -0,0 +1,13 @@
+package pl.fox.arcnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcNotesProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArcNotesProcessorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/fox/arcnotes/ArcNotesProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.fox.arcnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcNotesProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
