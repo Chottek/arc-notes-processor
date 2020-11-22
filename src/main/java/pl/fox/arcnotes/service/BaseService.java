9723e00bf662d39050557ab3740359db809e2d90
Chottek <49697145+Chottek@users.noreply.github.com>
2020-11-22 22:02:34 +0100

    Added a test class in which there are google api connections

--- src/main/java/pl/fox/arcnotes/service/BaseService.java
+++ src/main/java/pl/fox/arcnotes/service/BaseService.java
@@ -1,4 +1,27 @@
 package pl.fox.arcnotes.service;
 
+
+import com.google.cloud.vision.v1.ImageAnnotatorClient;
+import org.springframework.stereotype.Service;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
+@Service
 public class BaseService {
+
+    public void serve(){
+        try {
+            var image = ImageIO.read(new File("elo.png"));
+
+            var client = ImageAnnotatorClient.create();
+
+            var response = client.getStub();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
