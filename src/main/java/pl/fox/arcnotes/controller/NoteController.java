a29a4d17ed1fb9a562e3665b67e68864eac885f3
Chottek <49697145+Chottek@users.noreply.github.com>
2020-11-27 17:10:17 +0100

    Moved two methods to ImgConverter

--- src/main/java/pl/fox/arcnotes/controller/NoteController.java
+++ src/main/java/pl/fox/arcnotes/controller/NoteController.java
@@ -15,12 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.zip.DataFormatException;
-import java.util.zip.Deflater;
-import java.util.zip.Inflater;
+
 
 @RestController
 @RequestMapping("/api/notes/")
@@ -31,51 +27,8 @@ public class NoteController {
     @PostMapping("/upload")
     public BodyBuilder uplaodImage(@RequestParam("image") MultipartFile file) throws IOException {
         LOG.info("IMAGE BYTESIZE: {}", file.getBytes().length);
+
         return ResponseEntity.status(HttpStatus.OK);
-
     }
 
-    public static byte[] compressBytes(byte[] data) {
-        Deflater deflater = new Deflater();
-        deflater.setInput(data);
-        deflater.finish();
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
-        byte[] buffer = new byte[1024];
-        while (!deflater.finished()) {
-            int count = deflater.deflate(buffer);
-            outputStream.write(buffer, 0, count);
-        }
-        try {
-            outputStream.close();
-        } catch (IOException e) {
-            LOG.error("Something went wrong compressing data!");
-        }
-        LOG.info("Compressed Image - {}", outputStream.toByteArray().length);
-
-        return outputStream.toByteArray();
-
-    }
-
-
-        // uncompress the image bytes before returning it to the angular application
-
-    public static byte[] decompressBytes(byte[] data) {
-        Inflater inflater = new Inflater();
-        inflater.setInput(data);
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
-        byte[] buffer = new byte[1024];
-
-        try {
-            while (!inflater.finished()) {
-                int count = inflater.inflate(buffer);
-                outputStream.write(buffer, 0, count);
-            }
-            outputStream.close();
-        } catch (IOException | DataFormatException ioe) {
-            LOG.error("Something went wrong decompressing bytes");
-        }
-
-        return outputStream.toByteArray();
-
-    }
 }
