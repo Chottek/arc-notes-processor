ab7ffd491bfbfa793be0e9752b8189108f637b07
Chottek <49697145+Chottek@users.noreply.github.com>
2021-01-13 11:33:37 +0100

    Testing VisionController, added resource with FeatureType Label_Detection

--- src/main/java/pl/fox/arcnotes/controller/VisionController.java
+++ src/main/java/pl/fox/arcnotes/controller/VisionController.java
@@ -1,6 +1,33 @@
 package pl.fox.arcnotes.controller;
 
+import com.google.cloud.vision.v1.AnnotateImageResponse;
+import com.google.cloud.vision.v1.Feature;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gcp.vision.CloudVisionTemplate;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/vision/")
 public class VisionController {
+
+    private final CloudVisionTemplate template;
+    private final ResourceLoader loader;
+
+    @Autowired
+    public VisionController(CloudVisionTemplate template, ResourceLoader loader){
+        this.template = template;
+        this.loader = loader;
+    }
+
+    @RequestMapping("/getLabelDetection")
+    public String getLabelDetection(){
+
+        AnnotateImageResponse res = template.analyzeImage(loader.getResource("/note.jpg"), Feature.Type.LABEL_DETECTION);
+
+        return res.getLabelAnnotationsList().toString();
+    }
 
 
 
