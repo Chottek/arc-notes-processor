c6857767a10a8e5e02c6edb0dde27586c129b302
Chottek <49697145+Chottek@users.noreply.github.com>
2021-01-14 15:46:24 +0100

    Changed file extension, cos' it probably will be .wav

--- src/main/java/pl/fox/arcnotes/service/ProcessingService.java
+++ src/main/java/pl/fox/arcnotes/service/ProcessingService.java
@@ -7,62 +7,57 @@ import org.springframework.stereotype.Service;
 import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
+import java.io.File;
+import java.io.IOException;
+import java.io.SequenceInputStream;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ProcessingService {
 
     private static final Logger LOG = LoggerFactory.getLogger(ProcessingService.class);
 
-    private static final String FILE_EXT = "mp3";
+    private static final String FILE_EXT = "wav";
     private static final AudioFileFormat.Type FILE_TYPE = AudioFileFormat.Type.WAVE;
 
 
     /*
-    *
-    * @TODO: List<File> file = new ArrayList<>();
-    * @TODO: List<AudioInputStream> clips = new ArrayList<>();
-    * @TODO: file.forEach(e -> {clips.add(AudioSystem.getAudioInputStream(e))});
-    *
+    * @TODO:
+        File file = new File(filePath);
+        AudioInputStream clip = AudioSystem.getAudioInputStream(file); //THROWS EXCEPTION
+        List<File> file = new ArrayList<>();
+        List<AudioInputStream> clips = new ArrayList<>();
+        file.forEach(e -> {clips.add(AudioSystem.getAudioInputStream(e))});
     * */
 
 
-    private String returnFile() {
-
-        java.io.File file = new java.io.File(filePath);
-        AudioInputStream clip = AudioSystem.getAudioInputStream(file); //THROWS EXCEPTION
-
-        java.util.List<AudioInputStream> musicFiles = new java.util.ArrayList<>();
-        //@TODO: Figure a way to fill List with music to merge and return path to merged file
-
-        if (musicFiles.size() == 0 || musicFiles.size() == 1) {
+    private String returnFile(List<AudioInputStream> clips) {
+        if (clips.size() == 0 || clips.size() == 1) {
             return null;
         }
 
-        java.util.UUID randomUUID = java.util.UUID.randomUUID();
-        String response = randomUUID.toString().concat(FILE_EXT);
+        String res = UUID.randomUUID().toString().concat(FILE_EXT);
         AudioInputStream appendedFiles = null;
 
-        for (int i = 0; i < musicFiles.size() - 1; i++) {
+        for (int i = 0; i < clips.size() - 1; i++) {
             if (i == 0) {
                 appendedFiles = new AudioInputStream(
-                        new java.io.SequenceInputStream(musicFiles.get(i), musicFiles.get(i + 1)),
-                        musicFiles.get(i).getFormat(),
-                        musicFiles.get(i).getFrameLength() + musicFiles.get(i + 1).getFrameLength());
+                        new SequenceInputStream(clips.get(i), clips.get(i + 1)), clips.get(i).getFormat(),
+                        clips.get(i).getFrameLength() + clips.get(i + 1).getFrameLength());
                 continue;
             }
             appendedFiles = new AudioInputStream(
-                    new java.io.SequenceInputStream(appendedFiles, musicFiles.get(i + 1)),
-                            appendedFiles.getFormat(),
-                            appendedFiles.getFrameLength() + musicFiles.get(i + 1).getFrameLength());
+                    new SequenceInputStream(appendedFiles, clips.get(i + 1)), appendedFiles.getFormat(),
+                            appendedFiles.getFrameLength() + clips.get(i + 1).getFrameLength());
         }
         try {
-            AudioSystem.write(appendedFiles, FILE_TYPE, new java.io.File(response));
-        } catch (java.io.IOException ie) {
+            assert appendedFiles != null;
+            AudioSystem.write(appendedFiles, FILE_TYPE, new File(res));
+        } catch (IOException ie) {
             LOG.error("{}", ie.getMessage());
         }
-        return response;
-
-
+        return res;
     }
 
 
