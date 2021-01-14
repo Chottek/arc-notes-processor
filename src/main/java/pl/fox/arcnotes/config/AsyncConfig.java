2742dc4a158510eeda5eb17833cdf9761917a0e2
Chottek <49697145+Chottek@users.noreply.github.com>
2021-01-14 18:21:44 +0100

    Bumped into a problem with Google Authentication [Permission Denied] error

--- src/main/java/pl/fox/arcnotes/config/AsyncConfig.java
+++ src/main/java/pl/fox/arcnotes/config/AsyncConfig.java
@@ -7,23 +7,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-@Configuration
-@EnableAsync
-public class AsyncConfig {
-
-    private static final Logger LOG = LoggerFactory.getLogger(AsyncConfig.class);
-
-    @Bean(name = "taskExecutor")
-    public java.util.concurrent.Executor taskExecutor(){
-        LOG.info("Creating ASYNC TASK Executor");
-        final ThreadPoolTaskExecutor exec = new ThreadPoolTaskExecutor();
-        exec.setCorePoolSize(2);
-        exec.setMaxPoolSize(2);
-        exec.setQueueCapacity(100);
-        exec.setThreadNamePrefix("NoteThread-");
-        exec.initialize();
-        return exec;
-    }
-
-
-}
+//@Configuration
+//@EnableAsync
+//public class AsyncConfig {
+//
+//    private static final Logger LOG = LoggerFactory.getLogger(AsyncConfig.class);
+//
+//    @Bean(name = "taskExecutor")
+//    public java.util.concurrent.Executor taskExecutor(){
+//        LOG.info("Creating ASYNC TASK Executor");
+//        final ThreadPoolTaskExecutor exec = new ThreadPoolTaskExecutor();
+//        exec.setCorePoolSize(2);
+//        exec.setMaxPoolSize(2);
+//        exec.setQueueCapacity(100);
+//        exec.setAwaitTerminationSeconds(10);
+//        exec.setThreadNamePrefix("NoteThread-");
+//        exec.initialize();
+//        return exec;
+//    }
+//}
