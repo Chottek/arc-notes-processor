57aa895fb8c37a2206bdb97fbe0db9f87dea5753
Chottek <49697145+Chottek@users.noreply.github.com>
2021-01-15 20:11:31 +0100

    Removed unused imports (for now) <- Still struggling with google authentication -_-

--- src/main/java/pl/fox/arcnotes/ArcApplication.java
+++ src/main/java/pl/fox/arcnotes/ArcApplication.java
@@ -1,28 +1,15 @@
 package pl.fox.arcnotes;
 
-import com.google.auth.oauth2.GoogleCredentials;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.util.ResourceUtils;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+
 
 @SpringBootApplication
 public class ArcApplication {
 
-
-    public static void main(String[] args) throws IOException {
-        gAuth();
+    public static void main(String[] args){
         SpringApplication.run(ArcApplication.class, args);
     }
 
-    private static void gAuth() throws IOException {
-//        File accountJson = ResourceUtils.getFile("classpath:service-account.json");
-//        // You can specify a credential file by providing a path to GoogleCredentials.
-//        // Otherwise credentials are read from the GOOGLE_APPLICATION_CREDENTIALS environment variable.
-//        GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(accountJson));
-//        Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
-    }
 }
