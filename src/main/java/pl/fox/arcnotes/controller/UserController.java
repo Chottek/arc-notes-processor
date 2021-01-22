5c371d8422c17c0f643fec904c467e291ff0cf29
Chottek <49697145+Chottek@users.noreply.github.com>
2021-01-22 16:13:02 +0100

    Added UserController and Repository

---
+++ src/main/java/pl/fox/arcnotes/controller/UserController.java
@@ -0,0 +1,13 @@
+package pl.fox.arcnotes.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+
+
+
+}
---
+++ src/main/java/pl/fox/arcnotes/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package pl.fox.arcnotes.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.fox.arcnotes.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    //All things are in JpaRepository here
+}
