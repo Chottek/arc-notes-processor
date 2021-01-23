3a05d7682b2eb531a1cd33f9c6ba9dd8c34eacb7
Chottek <49697145+Chottek@users.noreply.github.com>
2021-01-23 11:23:43 +0100

    Added equals and hashcode methods

--- src/main/java/pl/fox/arcnotes/model/User.java
+++ src/main/java/pl/fox/arcnotes/model/User.java
@@ -1,6 +1,9 @@
 package pl.fox.arcnotes.model;
 
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * @author Chottek
  * User Entity to store image in database based on cookie-id
@@ -27,5 +30,29 @@ public class User {
 
     public void setFile(byte[] file) {
         this.file = file;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(cookieId, user.cookieId) &&
+                Arrays.equals(file, user.file);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(cookieId);
+        result = 31 * result + Arrays.hashCode(file);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "cookieId='" + cookieId + '\'' +
+                ", file length=" + file.length +
+                '}';
     }
 }
