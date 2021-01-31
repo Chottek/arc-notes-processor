1ca8581a4d1c08f4de7f32e78451e1b9a4a1a421
Chottek <49697145+Chottek@users.noreply.github.com>
2021-01-31 11:54:15 +0100

    Changed response in process method a little bit

--- src/main/java/pl/fox/arcnotes/controller/ClassifierController.java
+++ src/main/java/pl/fox/arcnotes/controller/ClassifierController.java
@@ -1,6 +1,8 @@
 package pl.fox.arcnotes.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import pl.fox.arcnotes.model.RequestEntity;
 import pl.fox.arcnotes.service.ProcessingService;
+
+import java.io.FileInputStream;
 
 /**
  * @author Chottek
@@ -33,14 +37,18 @@ public class ClassifierController {
      * @return ResponseEntity with music file or with return ERROR information
      */
     @PostMapping(value = "/process", consumes = {"multipart/form-data"})
-    public ResponseEntity<java.io.File> process(@ModelAttribute RequestEntity requestEntity, @RequestPart("photoFile") MultipartFile file){
+    public ResponseEntity<Resource> process(@ModelAttribute RequestEntity requestEntity, @RequestPart("photoFile") MultipartFile file){
         try{
             java.util.Optional<java.io.File> op = service.process(requestEntity);
 
             if(op.isPresent()){
-                return ResponseEntity.accepted()
-                        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + op.get().getName() + "\"")
-                        .body(op.get());
+                InputStreamResource resource = new InputStreamResource(new FileInputStream(op.get()));
+                HttpHeaders headers = new HttpHeaders(); headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename="+op.get().getName());
+                return ResponseEntity.ok()
+                        .headers(headers)
+                        .contentLength(op.get().length())
+                        .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                        .body(resource);
             }
         }catch(java.io.IOException ie){
             return ResponseEntity.unprocessableEntity().build();
