2290ad954892fe348ba330d08e2aef44d4c21000
Chottek <49697145+Chottek@users.noreply.github.com>
2021-01-31 14:15:47 +0100

    Added tests

--- src/test/java/pl/fox/arcnotes/ArcApplicationTests.java
+++ src/test/java/pl/fox/arcnotes/ArcApplicationTests.java
@@ -1,13 +1,43 @@
 package pl.fox.arcnotes;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+import pl.fox.arcnotes.model.Note;
+import pl.fox.arcnotes.repository.NoteRepository;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class ArcApplicationTests {
 
+    private final NoteRepository n_repo;
+
+    @Autowired
+    public ArcApplicationTests(NoteRepository n_repo) {
+        this.n_repo = n_repo;
+    }
+
     @Test
-    void contextLoads() {
+    public void checkNoteInit() {
+        Assert.notNull(n_repo.getNotes());
+        Assert.notEmpty(n_repo.getNotes());
+    }
+
+    @Test
+    public void checkIfMergeLessThan2ReturnsNull() throws IOException {
+        List<Note> notes = new ArrayList<>();
+        notes.add(n_repo.getNotes().get(0));
+
+        assert n_repo.merge(notes) == null;
+    }
+
+    @Test
+    public void checkIfMergerMoreThanOneReturnsFile() throws IOException {
+        assert n_repo.merge(n_repo.getNotes()) != null;
     }
 
 }
