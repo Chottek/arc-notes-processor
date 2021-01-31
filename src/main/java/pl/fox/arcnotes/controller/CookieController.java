7c23d8e2f9593dfe3d93070b03686a67fbcc2d84
Chottek <49697145+Chottek@users.noreply.github.com>
2021-01-31 13:26:28 +0100

    Added javadoc

--- src/main/java/pl/fox/arcnotes/controller/CookieController.java
+++ src/main/java/pl/fox/arcnotes/controller/CookieController.java
@@ -19,12 +19,23 @@ public class CookieController {
         this.service = service;
     }
 
+    /**
+     * Save [Add or save changes] method
+     * @param ce CookieEntity that contains cookieId and filebyte array
+     * @return ResponseEntity containing information that Object was saved
+     */
     @PostMapping
     public ResponseEntity<Void> save(@RequestBody CookieEntity ce){
         service.addOrSave(ce);
         return ResponseEntity.created(java.net.URI.create(URL + ce.getCookieId())).build();
     }
 
+
+    /**
+     * Method getting every Object that contains id given in param
+     * @param cookieID Id by which the objects will be gathered
+     * @return ResponseEntity with body of java.util.List containg CookieEntities
+     */
     @GetMapping("getByCookie/{id}")
     public ResponseEntity<java.util.List<CookieEntity>> getByCookie(@PathVariable("id") String cookieID){
         return java.util.Optional.ofNullable(service.findAllByCookieId(cookieID)).map(cookieEntities ->
@@ -32,6 +43,12 @@ public class CookieController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+
+    /**
+     * Method deleting every Object that contains id given in param
+     * @param cookieID id by which objects will be deleted
+     * @return ResponseEntity with successful deleting
+     */
     @DeleteMapping("deleteAllByCookie/{id}")
     public ResponseEntity<Void> delete(@PathVariable("id") String cookieID){
         service.removeAllByCookieId(cookieID);
